47334391169177cab66bf5ae0dd5d60dd0b63bdd
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-07 15:54:31 -0300

    Pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -98,7 +98,6 @@ public class UI {
 	
 //	printBoard (sobrecarregado) -> imprime o tabuleiro com as possiveis jogadas da peça selecionada
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves ){
-
 		for(int i = 0; i < pieces.length; i++) {
 				System.out.print(8 - i + " ");
 			for(int j = 0; j < pieces.length; j++) {
